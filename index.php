368ff75f66c3a01e657f8db64584f1305cf61ab9
Matt <matthieu.heurtin@gmail.com>
2015-07-26 12:50:56 -0400

    start archi

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <!-- En-tête de la page -->
+
+        <meta charset="utf-8" />
+
+        <title>COUCOUCOUCOUC</title>
+
+    </head>
+
+
+    <body>
+COMING SOON
+
+<br/>
+
+<a href="test.php">other page</a>
+    
+
+    <!-- Corps de la page -->
+
+    </body>
+
+</html>
