ee2a0d09685c9468d331846ab59a0f4f7dc2af14
matt-admin <matt-admin@matt-server.home>
2015-09-28 19:16:12 +0200

    save data

--- index.php
+++ index.php
@@ -20,18 +20,54 @@
 
 <p>
 	Hi,<br/>
-	You currently are on my personal server. <br/>
-	The purspose of this page is to show that I am able to manage an Apache Server.
+	You are, currently, on my personal server. <br/>
+	The purspose of this page is to show that I am able to manage an Apache Server. <br/>
+
 <?php
 $filename = "/home/matt-admin/webSite/compteur.txt";
 $compteur = file_exists($filename) ? file_get_contents($filename) + 1 : 1;
 file_put_contents($filename, $compteur, LOCK_EX);
-echo "Number of Visitor " $compteur;
+echo "Number of Visitor ";
+echo  $compteur;
 
+
+function getUserIP()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+
+
+$user_ip = getUserIP();
+
+
+
+
+ 
+$date = date('l jS \of F Y h:i:s A');
+
+$data = sprintf("%s %s %s\n", $compteur, $user_ip, $date);
+file_put_contents('/home/matt-admin/webSite/infos', $data, FILE_APPEND);
 
 ?>
+</p>
 
-</p> 
 
 
 
